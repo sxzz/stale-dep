8fef6850dee877421d85ca44744e6ecd95c14a0c
Alex <49969959+alexzhang1030@users.noreply.github.com>
2023-01-11 17:38:25 +0800

    feat: improve error handing (#1)

--- src/cli.ts
+++ src/cli.ts
@@ -9,21 +9,25 @@ const options = program.opts<{ update: boolean }>()
 run()
 
 async function run() {
-  const packageManager = await getPackageManager()
-  if (options.update) {
-    await storeHash(await calcHash(packageManager))
-    consola.success('Successfully store the dependency hash')
-  } else if (!(await checkHash(packageManager))) {
-    const installCommand = {
-      npm: 'npm install',
-      yarn: 'yarn',
-      pnpm: 'pnpm i',
-      'pnpm@6': 'pnpm i',
-      bun: 'bun install',
-    }[packageManager]
-    consola.error(
-      `Your node_modules is stale. Please run \`${installCommand}\` first.`
-    )
+  try {
+    const packageManager = await getPackageManager()
+    if (options.update) {
+      await storeHash(await calcHash(packageManager))
+      consola.success('Successfully store the dependency hash')
+    } else if (!(await checkHash(packageManager))) {
+      const installCommand = {
+        npm: 'npm install',
+        yarn: 'yarn',
+        pnpm: 'pnpm i',
+        'pnpm@6': 'pnpm i',
+        bun: 'bun install',
+      }[packageManager]
+      throw new Error(
+        `Your node_modules is stale. Please run \`${installCommand}\` first.`
+      )
+    }
+  } catch (err) {
+    consola.error((err as Error).message ?? 'Unknown error in stale-dep.')
     process.exit(1)
   }
 }
