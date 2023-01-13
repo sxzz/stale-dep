6c932c78c9d382e4b877586b22592385e956ea9b
markthree <1801982702@qq.com>
2023-01-13 15:54:39 +0800

    feat: check modifyTimeStamp before check cotent hash

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest'
-import { checkHash, getPackageManager } from '../src'
+import { checkHash, checkModifyTimeStamps, getPackageManager } from '../src'
 
 test('getPackageManager', async () => {
   expect(await getPackageManager()).toBe('pnpm')
@@ -8,4 +8,9 @@ test('getPackageManager', async () => {
 test('checkHash', async () => {
   const pm = await getPackageManager()
   expect(await checkHash(pm)).toBe(true)
+})
+
+test('checkModifyTimeStamps', async () => {
+  const pm = await getPackageManager()
+  expect(await checkModifyTimeStamps(pm)).toBe(true)
 })
