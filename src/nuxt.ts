7e770d17e70e3bd390535c996fb1384937130a4c
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-02-17 15:10:29 +0100

    feat: add nuxt module

---
+++ src/nuxt.ts
@@ -0,0 +1,21 @@
+import { defineNuxtModule } from '@nuxt/kit'
+import { check, getPackageManager } from '.'
+import type { PackageManager } from '.'
+
+export default defineNuxtModule<{
+  enabled?: boolean
+  packageManager?: PackageManager
+}>({
+  meta: {
+    name: 'stale-dep',
+    configKey: 'staleDep',
+  },
+  defaults: {
+    enabled: process.env.NODE_ENV !== 'test',
+  },
+  async setup(opts) {
+    if (opts.enabled) {
+      await check(opts.packageManager || (await getPackageManager()))
+    }
+  },
+})
