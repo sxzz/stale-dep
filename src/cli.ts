b93556bed83a46b79e6f99cb52f90ba367ce7068
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-02-03 05:42:51 +0800

    feat: improve cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,34 +1,41 @@
 import process from 'node:process'
 import cac from 'cac'
 import consola from 'consola'
+import { version } from '../package.json'
 import { check, update } from './command'
-import { PROJECT_NAME } from './constant'
+import { PROJECT_NAME, type PackageManager } from './constant'
 import { getPackageManager } from '.'
 
 const cli = cac(PROJECT_NAME)
+cli.help().version(version)
 
-cli.option('-p, --packageManager <packageManager>', 'specific package manager')
-cli.option('-u, --update', 'Update stale dependencies', { default: false })
-cli.option('-w, --warn', 'Show warning messages instead of errors', {
-  default: false,
-})
-const argv = cli.parse()
+cli
+  .option('-p, --packageManager <packageManager>', 'specific package manager')
+  .option('-u, --update', 'Update stale dependencies', { default: false })
+  .option('-w, --warn', 'Show warning messages instead of errors', {
+    default: false,
+  })
+  .command('', 'Check stale dependencies')
+  .action(run)
+cli.parse()
 
-run()
-
-async function run() {
+async function run(args: {
+  packageManager?: PackageManager
+  update: boolean
+  warn: boolean
+}) {
   try {
-    const pm = argv.options.packageManager || (await getPackageManager())
-    if (argv.options.update) {
+    const pm = args.packageManager || (await getPackageManager())
+    if (args.update) {
       await update(pm)
     } else {
       await check(pm)
     }
   } catch (error) {
-    consola[argv.options.warn ? 'warn' : 'error'](
+    consola[args.warn ? 'warn' : 'error'](
       `[${PROJECT_NAME}]`,
       (error as Error).message ?? `Unknown error in ${PROJECT_NAME}.`,
     )
-    if (!argv.options.warn) process.exit(1)
+    if (!args.warn) process.exit(1)
   }
 }
